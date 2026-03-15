54d67f6cd1215690bde35c03eaec284612e4e91f
landlawd <brian8319sch@proton.me>
2026-03-15 22:14:19 +0100

    Added new data
    Added new data for Galway City West, Cork South Central and Pembroke (D4)

---
+++ data/config_cork_south_central_ed.py
@@ -0,0 +1,40 @@
+# --------------------------------------------------
+# RTB SCRAPER INPUT DATA
+# 
+# 1) Reference property details: Eircode, Dwelling type, BER, Bedrooms and Floor size.
+
+# 2) Ranges to search for comparable properties: Eircodes (one or more), Dwelling type, BER, Bedrooms and Floor size
+#    The number of permutations determine the script runtime... be careful!
+# 
+# The code contains the possibility to set the local authority and electoral area from the config file, but this is not currently used.
+# To obtain these values, in the browser you must enter the eircode/address in step 1 and manually "inspect" the element in the page.
+# 
+# Reference values:
+# Note: Querying all of these would take a very long time, especially since we have some "timer waits" built into this script. 
+# ... plus a HIGH RISK of getting flagged as a DOS and getting IP-blocked.
+#   dwelling_types = ["100", "101"]  # 100=House, 101=Apartment / Flat
+#   BER values: 1=A1, 2=A2, 3=A3, 4=B1, 5=B2, 6=B3, 7=C1, 8=C2, 9=C3, 10=D1, 11=D2, 12=E1, 13=E2, 14=F, 15=G, 16=Exempt
+#   ber_values = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"]  
+#   bedrooms = [1,2,3,4,5] # or more
+#   floor_range = range(30,200) #  range is from x to y-1, e.g. 30 to 119
+#   floor_space_incr = 5  # increment floor space by 5 on each query, more efficient than incrementing every 1.
+# --------------------------------------------------
+
+reference_eircode = "T12F5Y0"
+# local_authority_value = "xx" # CORK CITY COUNTY COUNCIL
+# electoral_area_value = "xxxxxxx" # CORK CITY SOUTH CENTRAL
+reference_dwelling_type = "100"  # House
+reference_ber = "16" # Exempt
+reference_bedrooms = 1
+reference_floor_space = 40
+
+# 2) Comparable properties to query.
+# THE FIRST EIRCODE MUST BE THE SAME AS THE REFERENCE PROPERTY
+# AND THE OTHER EIRCODES MUST ALL BE IN THE SAME ELECTORAL AREA
+# (otherwise they won't be useful comparables!)
+comparable_eircodes = ["T12F5Y0", "T12F5W4", "T12X64V", "T12Y2AH","T12DTP2","T12K2F9","T12E9AC"]
+dwelling_types = ["100","101"]  # House, Apartment/Flat
+ber_values = ["3","5","6","7","9","11","16"] # A3,B2,B3,C1,C3,D2,Exempt
+bedrooms = [1]
+floor_range = range(30,50) # queries from low value to high, using the incremement below
+floor_space_incr = 5
